7c64c5e32a4ad509a0c95f40f42e92bd1a899b4e
Mark Boleigha <donhoenix@gmail.com>
2020-10-08 13:43:50 +0100

    exposed uiview extensions

--- WidgetUI/Sources/Extensions/UIView.swift
+++ WidgetUI/Sources/Extensions/UIView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-extension UIView {
+public extension UIView {
     
     func addShadow(color: UIColor? = UIColor(red: 0.27, green: 0.32, blue: 0.33, alpha: 1), withOffset offset: CGSize? = CGSize(width: 0, height: 2)) {
         self.layer.shadowOffset = offset!
