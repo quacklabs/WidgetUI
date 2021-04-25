083f8823dd17a93b0a4dba4b8ba3bfd5dad7652d
Mark Boleigha <donhoenix@gmail.com>
2021-04-25 09:34:47 +0100

    enhanced Text with autoscaling feature. applyying this commit will: make text scale their size based on the device/screen type. orientation was not taken into account in this implementation.

--- WidgetUI/Sources/UI/Label.swift
+++ WidgetUI/Sources/UI/Label.swift
@@ -20,6 +20,8 @@ public class Label: UILabel {
         }
     }
     
+
+
     @IBInspectable
     public var _font: UIFont = UIFont.systemFont(ofSize: 17) {
         didSet {
@@ -30,7 +32,7 @@ public class Label: UILabel {
     @IBInspectable
     public var attributes: [NSAttributedString.Key: Any] = [NSAttributedString.Key: Any]() {
         didSet {
-//            self.content?.a
+            //TODO: Make text re-render in IB or SWiftUI Accordingly
         }
     }
 
@@ -43,22 +45,57 @@ public class Label: UILabel {
     }
     
     override public func draw(_ rect: CGRect) {
-//        self.font = self.font?.dynamicSized()
         super.drawText(in: rect)
     }
     
-    public convenience init(font: UIFont?, content: NSMutableAttributedString?, color: UIColor?) {
+    public convenience init(font: UIFont?, content: NSMutableAttributedString?, color: UIColor? = .black) {
         self.init(frame: .zero)
         self.willSetConstraints()
-        self.font = font?.dynamicSized() ?? UIFont.systemFont(ofSize: 14).dynamicSized()
-        self.textColor = color ?? textColor
+        self.font = self.overrideFontSize(font: font!)
+        self.textColor = color
         self.lineBreakMode = .byWordWrapping
         self.numberOfLines = 0
-        
+        self.content = content
+        self.write() // show text with attribuutes applied
+        self.setNeedsDisplay()
+    }
+
+    func write() {
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineHeightMultiple = 0.99
         content?.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSRange(location: 0, length: content!.length))
+        self.text = nil
         self.attributedText = content
         self.setNeedsDisplay()
+        self.layoutIfNeeded()
+    }
+
+    func overrideFontSize(font: UIFont) -> UIFont {
+        let currentFontName = font.fontName
+        var calculatedFont: UIFont?
+        let weight = font.weight
+        let fontSize: CGFloat = font.pointSize
+        
+        switch UIDevice().model {
+        case .iPhoneSE, .iPhone5, .iPhone5S, .iPhone5C,
+             .iPod1, .iPod2, .iPod3, .iPod4, .iPod5, .iPhone4,
+             .iPhone4S:
+            calculatedFont = font
+        case .iPhone6, .iPhone6S, .iPhone7, .iPhone8:
+            calculatedFont = UIFont(name: currentFontName, size: fontSize * 1.08)!.withWeight(weight)
+        case .iPhone6plus, .iPhone6Splus, .iPhone7plus,
+             .iPhone8plus, .iPhoneX, .iPhoneXR, .iPhoneXS,
+             .iPhoneXSMax:
+            calculatedFont = UIFont(name: currentFontName, size: fontSize * 1.07)!.withWeight(weight)
+        case .iPad2, .iPad3, .iPad4, .iPad5, .iPad6, .iPadAir, .iPadMini,
+             .iPadPro2_12_9, .iPadAir2, .iPadMini2, .iPadMini3, .iPadMini4,
+             .iPadPro9_7, .iPadPro10_5, .iPadPro12_9:
+            calculatedFont =  UIFont(name: currentFontName, size: fontSize * 1.9)!.withWeight(weight)
+        case .AppleTV, .AppleTV_4K:
+            calculatedFont =  UIFont(name: currentFontName, size: fontSize * 3)!.withWeight(weight)
+        case .simulator, .unrecognized:
+           calculatedFont = font
+        }
+        return calculatedFont!
     }
 }
